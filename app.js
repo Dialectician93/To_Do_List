8b260fdc4655035164cf1f4641a65820f6a1deb6
Dialectician93 <sekolord99@gmail.com>
2023-02-06 15:04:10 +0300

    To-Do_List

---
+++ app.js
@@ -0,0 +1,104 @@
+const input = document.getElementById('input')
+const addBtn = document.querySelector('.fa-plus')
+const listMaker = document.querySelector(".list-maker");
+const h1 = document.querySelector('h1')
+let sec;
+const isEmpty = str => !str.trim().length
+let localCounter = localStorage.length
+
+console.log(localStorage);
+
+
+const localGet = () => {
+    Object.keys(localStorage).map(x => {
+        sec = document.createElement('section')
+        boxIcon = document.createElement('i')
+        boxIcon.className = "fa-regular fa-square"
+        xIcon = document.createElement('i')
+        xIcon.className = "fa-solid fa-xmark"
+        span = document.createElement('span')
+        content = document.createTextNode(localStorage.getItem(x));
+        span.appendChild(content)
+        sec.appendChild(boxIcon)
+        sec.appendChild(span)
+        sec.appendChild(xIcon)
+        listMaker.after(sec);
+        sec.addEventListener('click', (e) => {
+            if(e.target.classList.contains("fa-xmark")){
+                console.log(e.target.closest('section').id);
+                localStorage.removeItem(e.target.closest('section').id)
+                e.target.closest('section').remove()
+            }
+            else if(e.target.classList.contains("fa-regular")){
+                if(e.target.classList.contains("fa-square-check")){
+                    e.target.classList.add("fa-square")
+                    e.target.classList.remove("fa-square-check")
+                }
+                else{                
+                    e.target.classList.add("fa-square-check")
+                    e.target.classList.remove("fa-square")
+                }
+                e.target.closest("section").classList.toggle('dark')
+                e.target.nextElementSibling.classList.toggle('span-line')
+            };
+        })
+    })  
+}
+localGet()
+console.log(Array.from(localStorage));
+
+const sections = document.querySelectorAll('section')
+console.log(sections);
+
+Array.from(sections).map((x,i) => x.id = Object.keys(localStorage).reverse()[i])
+
+const days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+h1.innerText = `♥Happy ${days[(new Date().getDay())-1]}♥`
+
+addBtn.addEventListener('click', (e) => {
+    if(isEmpty(input.value)){
+        input.value = ''
+        return alert('Please Write Something ✏️') 
+    }
+    sec = document.createElement('section')
+    sec.setAttribute("id",`${localCounter}`)
+    boxIcon = document.createElement('i')
+    boxIcon.className = "fa-regular fa-square"
+    xIcon = document.createElement('i')
+    xIcon.className = "fa-solid fa-xmark"
+    span = document.createElement('span')
+    content = document.createTextNode(input.value);
+    span.appendChild(content)
+    sec.appendChild(boxIcon)
+    sec.appendChild(span)
+    sec.appendChild(xIcon)
+    listMaker.after(sec);
+    console.log(input.value);
+    localStorage.setItem((localCounter),input.value)
+    console.log(localStorage);
+    input.value = ''
+    sec.addEventListener('click', (e) => {
+        if(e.target.classList.contains("fa-xmark")){
+            console.log(e.target.closest('section').id);
+            localStorage.removeItem(e.target.closest('section').id)
+            e.target.closest('section').remove()
+        }
+        else if(e.target.classList.contains("fa-regular")){
+            if(e.target.classList.contains("fa-square-check")){
+                e.target.classList.add("fa-square")
+                e.target.classList.remove("fa-square-check")
+            }
+            else{                
+                e.target.classList.add("fa-square-check")
+                e.target.classList.remove("fa-square")
+            }
+            e.target.closest("section").classList.toggle('dark')
+            e.target.nextElementSibling.classList.toggle('span-line')
+        };
+    })
+    localCounter++
+})
+
+window.addEventListener('keyup', (e) => {
+    e.code === 'Enter' && addBtn.click()
+})
